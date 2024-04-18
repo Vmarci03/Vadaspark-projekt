d3894b2e95eec4081d97880f6e25a84a7543208b
Lightning 77 yt <villamnyul77@gmail.com>
2024-04-18 11:42:38 +0200

    Apró változtatások.

--- C#/Vadaspark/Vadaspark/MainWindow.xaml.cs
+++ C#/Vadaspark/Vadaspark/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Vadaspark
 {
@@ -23,6 +24,23 @@ namespace Vadaspark
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void closeapp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void datawritedown_Click(object sender, RoutedEventArgs e)
+        {
+
+            StreamWriter sw = new StreamWriter("nyilvantartas.txt",true);
+
+            sw.WriteLine("Állat neve: {0}\nÁllat fajtája: {1}\nSzületési idő:{2}\nNőstény: {3}/Hím: {4}");
+
+            sw.Flush();
+            sw.Close();
+
         }
     }
 }
