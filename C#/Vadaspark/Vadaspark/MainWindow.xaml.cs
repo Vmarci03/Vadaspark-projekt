6cd131d0e3045183ad115e08976046964815b98e
Lightning 77 yt <villamnyul77@gmail.com>
2024-04-18 12:42:32 +0200

    Grafikus felület elkészítve.

--- C#/Vadaspark/Vadaspark/MainWindow.xaml.cs
+++ C#/Vadaspark/Vadaspark/MainWindow.xaml.cs
@@ -36,11 +36,39 @@ namespace Vadaspark
 
             StreamWriter sw = new StreamWriter("nyilvantartas.txt",true);
 
-            sw.WriteLine("Állat neve: {0}\nÁllat fajtája: {1}\nSzületési idő:{2}\nNőstény: {3}/Hím: {4}");
+            sw.WriteLine("Állat neve: {0}\nÁllat fajtája: {1}\nSzületési idő:{2}\nNőstény: {3}/Hím: {4}\nSzármazási hely: {5}\nVédett állat-e: igen: {6}/nem:{7}\nOsztály: Hüllők:{8}/Kétéltűek:{9}/Madarak:{10}/Emlősök:{11}\n\n", animalname.Text, animaltype.Text, birthday.Text, type_female.IsChecked, type_male.IsChecked, Place_of_origin.Text, yes_protected.IsChecked, not_protected.IsChecked, reptilian.IsChecked, amphibious.IsChecked, bird.IsChecked, mammals.IsChecked);
 
             sw.Flush();
             sw.Close();
 
+            amount.Content = animallist.Items.Count;
+
+            animalname.Text = "";
+            animaltype.Text = "";
+            birthday.Text = "";
+            type_male.IsChecked = false;
+            type_female.IsChecked = false;
+            Place_of_origin.Text = "";
+            yes_protected.IsChecked = false;
+            not_protected.IsChecked = false;
+            reptilian.IsChecked = false;
+            amphibious.IsChecked = false;
+            bird.IsChecked = false;
+            mammals.IsChecked = false;
+        }
+
+        private void deletebtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (animallist.SelectedIndex == -1)
+            {
+
+                MessageBox.Show("Nincs kijelölve egy halott állat sem!","Figyelmeztetés");
+
+            }
+            else
+            {
+                animallist.Items.RemoveAt(animallist.SelectedIndex);
+            }
         }
     }
 }
